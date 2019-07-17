247249392d042a922a5ca49cd0d834821104ae91
Weverton Rodrigues <wevertonmixalt@gmail.com>
2019-07-17 09:16:44 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,21 @@
-//pega km 
-let x = document.getElementsByClassName("Us(t) Va(m) D(ib) My(2px) NetWidth(100%,20px) C(#fff) Ell");
-let arrayX = [].slice.call(x);
-let y = arrayX.pop()
-let z = y.getElementsByTagName("span");
-let distanceKm = z[0].innerText;
-let distanceKmString = distanceKm.toString();
-
 function myFunction() {
-    setTimeout(() => {
+
+    //pega km 
+    let x = document.getElementsByClassName("Us(t) Va(m) D(ib) My(2px) NetWidth(100%,20px) C(#fff) Ell");
+    let arrayX = [].slice.call(x);
+    let y = arrayX.pop()
+    let z = y.getElementsByTagName("span");
+    let distanceKm = z[0].innerText;
+
     //btn nao da like
     let notBtn = document.querySelector('[aria-label="Não"]');
     //btn da like
     let yesBtn = document.querySelector('[aria-label="Curti"]');
-        if(distanceKmString != "a 2 quilômetros daqui"){
-            yesBtn.click();
-        }else{
-            notBtn.click(); 
-        }
-    }, 3000);
+
+    if (distanceKm == "a 2 quilômetros daqui") {
+        notBtn.click();
+    } else {
+        yesBtn.click();
+    }
 }
 setInterval(myFunction, 6000);
